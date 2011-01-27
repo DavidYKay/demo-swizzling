cdccb907becff643689d4539e3cb49ee062b3d2c
David Young-Chan Kay <davidykay@gmail.com>
2011-01-27 11:31:23 -0500

    Demonstrates behavior of a category.

---
+++ Classes/ObjectCategory.m
@@ -0,0 +1,17 @@
+//
+//  ObjectCategory.m
+//  Swizzling
+//
+//  Created by David Young-Chan Kay on 1/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "OriginalObject.h"
+
+@implementation OriginalObject (Additions)
+
+- (NSString *)hello {
+	return @"fhqwhgads";
+}
+
+@end
